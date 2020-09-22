02ff2fe566568822df387e9fcce9cafde0056f6b
woodyvalentino123 <69005803+woodyvalentino123@users.noreply.github.com>
2020-09-22 18:01:34 +0530

    Add files via upload

--- Food.js
+++ Food.js
@@ -6,52 +6,34 @@ class Food {
         this.foodStock = 0;
         this.lastFed ;
     }
-    getstock(){
-        database = firebase.database();
-    var getFoodStock = database.ref("Food");
-     getFoodStock.on("value",function(data){
-         this.foodStock = data.val();
-     })
-    }
-    getTime(){
-        database1 = firebase.database();
-        var getTime = database.ref("FeedTime");
-        getTime.on("value",function(data){
-        this.lastfed = data.val();
-            
-        })
-    }
+   
+       
+    getFedTime(lastFed){
+      this.lastFed=lastFed;   
+       }
     
     updateFoodStock(foodStock){
-    this.foodStock =this.foodStock+1;
-    database.ref("/").update({
-    Food:foodStock
-  })
+    this.foodStock = foodStock;
+    
+  }
 
-    }
-    deductFoodStock(foodStock){
-        if(foodStock<=0){
-            foodStock=0;
-          }else{
-            foodStock=foodStock-1;
+    
+    deductFoodStock(){
+        if(this.foodStock>0){
+        this.foodStock = this.foodStock-1;
           }
-            database.ref('/').update({
-              Food:foodStock,
-              
-            })
-          }
-          updateTime(hour){
-              database.ref('/').update({
-                  FeedTime:hour
-              })
-          }
+        }
+          
+        getFoodStock(){
+            return this.foodStock;
+        }
     
      
     display(){
         var x =80,y=100;
         imageMode(CENTER);
         image(this.image,720,220,70,70);
-        if(this.foodStock.getStock!==0){
+        if(this.foodStock!==0){
             for(var i =0;i<this.foodStock;i++){
                 if(i%10===0){
                     x = 80;
--- sketch.js
+++ sketch.js
@@ -1,72 +1,69 @@
 //Create variables here
-var dog, happyDog,database,foods,foodStock;
+var doghungry, happyDog,database,foods,foodStock;
 var img1,img2;
 var lastfed,milk;
-var hour;
-
+var time;
+var button1,button2;
+var happyD,hungryD;
+var getFoodStock;
 function preload()
 {
  
   //load images here
-  img1 = loadImage("images/dogImg.png");
-  img2 = loadImage("images/dogImg1.png");
-  food1 = loadImage("images/Milk.png");
+  happyD = loadImage("images/dogImg.png");
+  hungryD = loadImage("images/dogImg1.png");
+  
 }
 
 function setup() {
   createCanvas(500, 500);
-  database = firebase.database();
-  
-  dog = createSprite(400,200,20,20);
-  
-  happyDog = createSprite(100,100,20,20);
- 
-  milk = new Food();
+  happyDog = createSprite(100,100,20,20); 
+  doghungry = createSprite(400,200,20,20);
+  doghungry.addImage(hungryD);
+  doghungry.scale=0.2;
 
-  milk.updateTime(hour)
- 
+  database = firebase.database();
+  getFoodStock = database.ref("Food");
+  getFoodStock.on("value",readStock,showerror);
+   
+    foodobj = new Food();
+
+  button1 = createButton('Feed the Dog');
+  button2 = createButton('Add the Food');
+
+  button1.position(700,95);
+  button2.position(800,95);
+
+  button1.mousePressed(feedDog);
+  button2.mousePressed(addFoods)
   
-  
-  
-  
-}
+  }
 
 
 function draw() {  
   background(46,139,87);
-  var button1 = createButton('Feed the Dog');
-    var button2 = createButton('Add the Food');
-    button1.position(700,100);
-    button2.position(700,60);
-    console.log(hour);
-  
- button1.mousePressed(function(){
-   deduct = new Food();
-    deduct.deductFoodStock(foodStock);
-    dog.scale = 0.2;
+  foodobj.display();
 
- })
-       
-   button2.mousePressed(function(){
-    var  updt= new Food();
-    updt.updateFoodStock(foodStock);
-    happyDog.addImage(img1);
-    happyDog.scale = 0.2;
- })
- 
+
+  feedTime = database.ref("FeedTime");
+  feedTime.on("value",function(data){
+   lastFed = data.val();
+  })
+
+  
+fill(225);
+textSize(15);
+if(lastFed>=12) {
+  text("Last Fed :" + lastFed%12 + "PM",350,30)
+} 
+else if (lastFed === 0){
+  text("Last Fed : 12  AM",350,30);
+}else {
+  text("Last Fed : "+ lastFed + "AM",350,30);
+}
+   
   drawSprites();
-  
-  
-  dog.addImage(img2)
-  dog.scale=0.2;
-  milk.display();
-  
-  textSize(15);
-  fill('red')
-  stroke(4)
-  text("Last Fed :" + time,80,40);
-  //add styles here
-  
+   
 
 }
 
@@ -77,13 +74,32 @@ function showerror(){
 
 }
 
-async function update(hour){
-  var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  async function hour(){
+  var response =  await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
     var responseJSON = await response.json();
 
     var datetime = responseJSON.datetime;
-     hour = datetime.slice(11,13);
+     time = datetime.slice(11,13);
+     console.log(time);
+  }
+
+function readStock(data){
+  foodS= data.val();
+  foodobj.updateFoodStock(foods)
+
+}
+function feedDog(){
+  dog.addImage(happyD);
+  foodobj.updateFoodStock(foodobj.getFoodStock()-1);
+  database.ref("/").update({
+  Food:foodobj.getFoodStock(),
+  FeedTime:hour(time)
+})
 }
 
-
-
+function addFoods(){
+  foodS++;
+  database.ref("/").update({
+    Food:foodS
+  })
+}
